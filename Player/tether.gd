ed5395423d4a6f9e5e87698fb835d60fd40b340b
Nova840 <adamzmudzinski2@gmail.com>
2026-01-31 14:27:29 -0500

    Add collision to air tank

--- Player/tether.gd
+++ Player/tether.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends RigidBody3D
 class_name Tether
 
 
@@ -9,6 +9,9 @@ class_name Tether
 @export var stiffness := 40.0
 @export var damping := 6.0
 
+@export var air_tank_max_velocity: float
+@export var tube_length: float = 2.0
+
 @onready var tube_1: Node3D = $Tube1
 @onready var tube_2: Node3D = $Tube2
 
@@ -17,12 +20,12 @@ func _physics_process(delta: float) -> void:
 	var dir := body_b.global_position - body_a.global_position
 	var distance := dir.length()
 
-	var middle_position := (body_b.global_position + body_a.global_position) / 2.0
-	global_position = middle_position
 	global_basis = Basis.looking_at(dir.cross(Vector3.UP), Vector3.UP)
-	
-	tube_1.scale.z = distance / 4.0 # half the distance times the tube length (2)
-	tube_2.scale.z = distance / 4.0
+
+	tube_1.look_at(body_a.global_position)
+	tube_1.scale.z = tube_1.global_position.distance_to(body_a.global_position) / tube_length
+	tube_2.look_at(body_b.global_position)
+	tube_2.scale.z = tube_2.global_position.distance_to(body_b.global_position) / tube_length
 
 	if distance <= max_length:
 		return
@@ -39,3 +42,11 @@ func _physics_process(delta: float) -> void:
 
 	body_a.apply_central_force(force)
 	body_b.apply_central_force(-force)
+
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var middle_position := (body_b.global_position + body_a.global_position) / 2.0
+	var vel := (middle_position - global_position) / get_physics_process_delta_time()
+	vel = vel.limit_length(air_tank_max_velocity)
+	state.linear_velocity = vel
+	state.angular_velocity = Vector3.ZERO

a54336fe646b2408defc04ce7111255d060fe95d
Nova840 <adamzmudzinski2@gmail.com>
2026-01-31 18:17:36 -0500

    Add janitor models

--- Player/player_rigid_body.gd
+++ Player/player_rigid_body.gd
@@ -12,12 +12,19 @@ class_name PlayerRigidBody
 @export var input_down: String
 @export var input_add_force: String
 
+@onready var l_pipe_rotate_point: Node3D = $"Janitor Model/LPipeRotatePoint"
+
+var previous_global_basis: Basis
+
 
 func _physics_process(delta: float) -> void:
 	var input_vector := Input.get_vector(input_left, input_right, input_down, input_up)
 	if input_vector != Vector2.ZERO:
 		var target_global_basis = Basis.looking_at(Vector3(input_vector.x, 0, -input_vector.y), Vector3.UP)
 		global_basis = global_basis.slerp(target_global_basis.orthonormalized(), rotate_speed * delta)
+		previous_global_basis = global_basis
+	else:
+		global_basis = previous_global_basis
 	var force_strength := Input.get_action_strength(input_add_force)
 	apply_central_force(-global_basis.z * (rocket_strength * force_strength))
 
@@ -26,4 +33,3 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if state.linear_velocity.length() > max_velocity:
 		state.linear_velocity = state.linear_velocity.normalized() * max_velocity
 	state.angular_velocity = Vector3.ZERO
-#func _on_body_entered(): print("ASd")
--- Player/tether.gd
+++ Player/tether.gd
@@ -2,8 +2,8 @@ extends RigidBody3D
 class_name Tether
 
 
-@export var body_a: RigidBody3D
-@export var body_b: RigidBody3D
+@export var body_a: PlayerRigidBody
+@export var body_b: PlayerRigidBody
 
 @export var max_length := 5.0
 @export var stiffness := 40.0
@@ -22,10 +22,13 @@ func _physics_process(delta: float) -> void:
 
 	global_basis = Basis.looking_at(dir.cross(Vector3.UP), Vector3.UP)
 
-	tube_1.look_at(body_a.global_position)
-	tube_1.scale.z = tube_1.global_position.distance_to(body_a.global_position) / tube_length
-	tube_2.look_at(body_b.global_position)
-	tube_2.scale.z = tube_2.global_position.distance_to(body_b.global_position) / tube_length
+	tube_1.look_at(body_a.l_pipe_rotate_point.global_position)
+	tube_1.scale.z = tube_1.global_position.distance_to(body_a.l_pipe_rotate_point.global_position) / tube_length
+	tube_2.look_at(body_b.l_pipe_rotate_point.global_position)
+	tube_2.scale.z = tube_2.global_position.distance_to(body_b.l_pipe_rotate_point.global_position) / tube_length
+	
+	body_a.l_pipe_rotate_point.global_basis = Basis.looking_at((global_position - body_a.global_position).cross(Vector3.UP), Vector3.UP)
+	body_b.l_pipe_rotate_point.global_basis = Basis.looking_at((global_position - body_b.global_position).cross(Vector3.UP), Vector3.UP)
 
 	if distance <= max_length:
 		return
@@ -45,7 +48,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	var middle_position := (body_b.global_position + body_a.global_position) / 2.0
+	var middle_position := (body_b.l_pipe_rotate_point.global_position + body_a.l_pipe_rotate_point.global_position) / 2.0
 	var vel := (middle_position - global_position) / get_physics_process_delta_time()
 	vel = vel.limit_length(air_tank_max_velocity)
 	state.linear_velocity = vel

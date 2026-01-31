1d74dbd1c79336516237ba2023e6bdbe31ca972c
Nova840 <adamzmudzinski2@gmail.com>
2026-01-31 14:54:06 -0500

    Add text display for oxygen time

--- Player/players.gd
+++ Player/players.gd
@@ -5,8 +5,11 @@ class_name Players
 @export var starting_oxygen_time: float
 @export var damaged_oxygen_time_loss: float
 
+@onready var temporary_oxygen_label: Label = $TemporaryOxygenLabel
+
 var oxygen_time: float
 var players_array: Array[PlayerRigidBody]
+var oxygen_depleted: bool = false
 
 
 func _ready() -> void:
@@ -20,6 +23,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	oxygen_time -= delta
+	oxygen_time = max(oxygen_time, 0)
+	if oxygen_time <= 0 and not oxygen_depleted:
+		oxygen_depleted = true
+		print("Lose")
+	temporary_oxygen_label.text = "Oxygen: " + str(ceili(oxygen_time)) + "/" + str(ceili(starting_oxygen_time))
 
 
 func _player_damaged() -> void:

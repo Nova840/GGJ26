0f43902cdd0a718441aaf0e7484a278abb1eca5f
Nova840 <adamzmudzinski2@gmail.com>
2026-02-01 11:28:51 -0500

    Title screen controller support and main scene

--- scene_change.gd
+++ scene_change.gd
@@ -4,6 +4,11 @@ extends Node
 @export var click_sound: AudioStream
 
 
+func _process(delta: float) -> void:
+	if Input.get_action_strength("AddForce1") >= 1 or Input.get_action_strength("AddForce1") >= 1:
+		_on_pressed()
+
+
 func _on_pressed():
 	Sound.play(click_sound)
 	SceneTransition.change_scene("res://Level/Level.tscn")

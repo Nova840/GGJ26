4779e5b7eaf7f74116d070355c12d0fc2a4eb541
Nova840 <adamzmudzinski2@gmail.com>
2026-01-31 20:59:57 -0500

    Fix starting game from level scene

--- scenes/scene_transition.gd
+++ scenes/scene_transition.gd
@@ -2,6 +2,11 @@ extends CanvasLayer
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var color_rect: ColorRect = $ColorRect
+
+
+func _ready() -> void:
+	color_rect.color = Color.TRANSPARENT
 
 
 func change_scene(file: String) -> void:

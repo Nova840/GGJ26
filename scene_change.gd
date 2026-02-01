bad9d8c8196e927cacaae9aa6a01d793f8274f87
Nova840 <adamzmudzinski2@gmail.com>
2026-02-01 11:36:34 -0500

    Make quit game a global

--- scene_change.gd
+++ scene_change.gd
@@ -7,8 +7,6 @@ extends Node
 func _process(delta: float) -> void:
 	if Input.get_action_strength("AddForce1") >= 1 or Input.get_action_strength("AddForce1") >= 1:
 		_on_pressed()
-	if Input.get_action_strength("Exit") >= 1:
-		get_tree().quit()
 
 
 func _on_pressed():

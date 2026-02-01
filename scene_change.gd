ec25548a20ff3233ae1e134c3bfbd6302121da36
Nova840 <adamzmudzinski2@gmail.com>
2026-02-01 13:40:44 -0500

    Add delay to start and end screens

--- scene_change.gd
+++ scene_change.gd
@@ -3,10 +3,19 @@ extends Node
 
 @export var scene_to_load: String
 @export var click_sound: AudioStream
+@export var click_delay: float
+
+var time_created: float
+
+
+
+func _ready() -> void:
+	time_created = Time.get_ticks_msec() / 1000.0
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("AddForce1") or Input.is_action_just_pressed("AddForce1") :
+	if (Input.is_action_just_pressed("AddForce1") or Input.is_action_just_pressed("AddForce1")) \
+			and Time.get_ticks_msec() / 1000.0 - time_created >= click_delay:
 		_on_pressed()
 
 

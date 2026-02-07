e701ad69d4c2b019e43eb994bc9b49b1c6b54e3a
Nova840 <adamzmudzinski2@gmail.com>
2026-02-06 19:57:41 -0500

    Credits Menu

--- scenes/scene_change.gd
+++ scenes/scene_change.gd
@@ -1,22 +1,31 @@
-extends Node
+extends Button
 
 
 @export var scene_to_load: String
 @export var click_sound: AudioStream
 @export var click_delay: float
 
-var time_created: float
+@export var focus_on_ready: bool = true
 
+@export var press_on_input: Array[String]
+
+var time_created: float
 
 
 func _ready() -> void:
 	time_created = Time.get_ticks_msec() / 1000.0
+	if focus_on_ready:
+		grab_focus()
 
 
 func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("AddForce1") or Input.is_action_just_pressed("AddForce2")) \
-			and Time.get_ticks_msec() / 1000.0 - time_created >= click_delay:
+	if Time.get_ticks_msec() / 1000.0 - time_created < click_delay:
+		return
+	if (Input.is_action_just_pressed("AddForce1") or Input.is_action_just_pressed("AddForce2")) and has_focus():
 		_on_pressed()
+	for input in press_on_input:
+		if Input.is_action_just_pressed(input):
+			_on_pressed()
 
 
 func _on_pressed():

377e3f71624b0eb3479e1a15f9e01218d9d80b25
Nova840 <adamzmudzinski2@gmail.com>
2026-02-13 20:24:05 -0500

    Remove quit game on escape from web builds

--- scenes/quit_game.gd
+++ scenes/quit_game.gd
@@ -2,5 +2,5 @@ extends Node
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Exit"):
+	if Input.is_action_just_pressed("Exit") and not OS.has_feature("web"):
 		get_tree().quit()
